a4b9a2e23179bf776342764e89686553874e46ba
Danesh Muthu Krisnan <37767781+DaneshIV@users.noreply.github.com>
2024-11-13 17:12:38 +0800

    DJAAS-53   Bug Fixing (Issue : File Corruption & Hacked Files #1)
    Fixing API Issue and Login Issue with file corruption

---
+++ dittojenderamautoworks/client/clerk-react/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { styled, Box } from "@mui/material";
+
+// Flex Between
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
